58fbe9cfaff32ee79fc9b04cf79076cb5b66a728
fogus <mefogus@gmail.com>
2012-03-15 17:07:59 -0400

    bumped version

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
-(defproject bacwn "0.0.0"
+(defproject bacwn "0.1.0"
   :description "Deep studies in Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-marginalia "0.7.0"]
-                     [swank-clojure "1.4.0"]])
+                     [swank-clojure "1.4.0"]
+                     [lein-multi "1.1.0"]]
+  :multi-deps {"1.2" [[org.clojure/clojure "1.2.0"]]
+               "1.3" [[org.clojure/clojure "1.3.0"]]
+               "1.4" [[org.clojure/clojure "1.4.0-beta1"]]})
