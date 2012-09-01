71e44f56da1808b4df28ad00dfae8ee8082ef466
fogus <mefogus@gmail.com>
2012-09-01 08:45:51 -0400

    EL talk version

--- src/bacwn/datalog/enghraifft.clj
+++ src/bacwn/datalog/enghraifft.clj
@@ -22,32 +22,34 @@
              {:location/db.id  0 :location/character 1 :location/name "SoL"}
              {:location/db.id  1 :location/character 2 :location/name "Gizmonics"})))
 
-(def rules
+(def locate-rule
   (rules-set
    (<- (:stationed-at :location/name ?loc-name :character/name ?char-name)
-       (:location  :location/name ?loc-name :location/character ?char)
-       (:character :character/db.id ?char :character/name ?char-name))))
+       (:location  :name ?loc-name :character ?char)
+       (:character :character/db.id ?char :name ?char-name))))
 
-(def wp-1 (build-work-plan rules (?- :stationed-at :location/name '??loc :character/name ?char-name)))
-(run-work-plan wp-1 mst3k-db {'??loc "SoL"})
+(run-work-plan
+ (build-work-plan locate-rule
+   (?- :stationed-at :location/name '??loc :character/name ?char-name))
+ mst3k-db
+ {'??loc "SoL"})
+;; ({:location/name "SoL", :character/name "Crow"} {:location/name "SoL", :character/name "Joel"})
 
 
-(def rules2
+(def non-human-locate-rule
   (rules-set
-   (<- (:works-for :employee ?x :boss ?y) (:boss :employee-id ?e-id :boss-id ?b-id)
-       (:employee :id ?e-id :name ?x)
-       (:employee :id ?b-id :name ?y))
-   (<- (:works-for :employee ?x :boss ?y) (:works-for :employee ?x :boss ?z)
-       (:works-for :employee ?z :boss ?y))
-   (<- (:employee-job* :employee ?x :job ?y) (:employee :name ?x :position ?pos)
-       (:can-do-job :position ?pos :job ?y))
-   (<- (:employee-job* :employee ?x :job ?y) (:job-replacement :job ?y :can-be-done-by ?z)
-       (:employee-job* :employee ?x  :job ?z))
-   (<- (:employee-job* :employee ?x :job ?y) (:can-do-job :job ?y)
-       (:employee :name ?x :position ?z)
-       (if = ?z :boss))
-   (<- (:employee-job :employee ?x :job ?y) (:employee-job* :employee ?x :job ?y)
-       (:employee :id ?id :name ?x)
-       (not! :job-exceptions :id ?id :job ?y))
-   (<- (:bj :name ?x :boss ?y) (:works-for :employee ?x :boss ?y)
-       (not! :employee-job :employee ?y :job :pc-support))))
+   (<- (:stationed-at :location/name ?loc-name :character/name ?char-name)
+       (:location  :name ?loc-name :character ?char)
+       (:character :character/db.id ?char :name ?char-name)
+       (not! :character :character/db.id ?char :human? true))))
+
+(run-work-plan
+ (build-work-plan non-human-locate-rule
+   (?- :stationed-at :location/name '??loc :character/name ?char-name))
+ mst3k-db
+ {'??loc "SoL"})
+
+(q (?- :stationed-at :location/name '??loc :character/name ?char-name)
+   mst3k-db
+   non-human-locate-rule
+   {'??loc "SoL"})
--- src/bacwn/datalwg.clj
+++ src/bacwn/datalwg.clj
@@ -20,58 +20,9 @@
   `(add-tuples ~db
     ~@(map explode tuples)))
 
-(comment
-  (macroexpand '(facts nil
-                       {:employee/db.id 1 :employee/name "Bob" :employee/position :boss}
-                       {:employee/db.id 1 :employee/name "Henry" :employee/position :nug}))
-
-  (explode
-   {:employee/db.id 1
-    :employee/name "Bob"
-    :employee/position :boss})
-
-  ;;=> ([:employee :db/id 1 :name "Bob"] [:employee :db/id 1 :position :boss])
-)
-
-(comment
-
-(def db
-  (add-tuples
-   db-base
-   [:employee :id 1  :name "Bob"    :position :boss]
-   [:employee :id 2  :name "Mary"   :position :chief-accountant]
-   [:employee :id 3  :name "John"   :position :accountant]
-   [:employee :id 4  :name "Sameer" :position :chief-programmer]
-   [:employee :id 5  :name "Lilian" :position :programmer]
-   [:employee :id 6  :name "Li"     :position :technician]
-   [:employee :id 7  :name "Fred"   :position :sales]
-   [:employee :id 8  :name "Brenda" :position :sales]
-   [:employee :id 9  :name "Miki"   :position :project-management]
-   [:employee :id 10 :name "Albert" :position :technician]
-   
-   [:boss :employee-id 2  :boss-id 1]
-   [:boss :employee-id 3  :boss-id 2]
-   [:boss :employee-id 4  :boss-id 1]
-   [:boss :employee-id 5  :boss-id 4]
-   [:boss :employee-id 6  :boss-id 4]
-   [:boss :employee-id 7  :boss-id 1]
-   [:boss :employee-id 8  :boss-id 7]
-   [:boss :employee-id 9  :boss-id 1]
-   [:boss :employee-id 10 :boss-id 6]
-   
-   [:can-do-job :position :boss               :job :management]
-   [:can-do-job :position :accountant         :job :accounting]
-   [:can-do-job :position :chief-accountant   :job :accounting]
-   [:can-do-job :position :programmer         :job :programming]
-   [:can-do-job :position :chief-programmer   :job :programming]           
-   [:can-do-job :position :technician         :job :server-support]
-   [:can-do-job :position :sales              :job :sales]
-   [:can-do-job :position :project-management :job :project-management]
-   
-   [:job-replacement :job :pc-support :can-be-done-by :server-support]
-   [:job-replacement :job :pc-support :can-be-done-by :programming]
-   [:job-replacement :job :payroll    :can-be-done-by :accounting]
-   
-   [:job-exceptions :id 4 :job :pc-support]))
-  
-)
+(defn q
+  [query db rules bindings]
+  (run-work-plan
+   (build-work-plan rules query)
+   db
+   bindings))
