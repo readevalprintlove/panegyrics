daa594207fd41b7f18a7c14db5b10d9e5d1d5139
fogus <mefogus@gmail.com>
2012-10-11 22:24:08 -0400

    refactoring

--- clojure/bacwn/src/main/clojure/clj/bacwn/datalog/impl/magic.clj
+++ clojure/bacwn/src/main/clojure/clj/bacwn/datalog/impl/magic.clj
@@ -15,10 +15,10 @@
 ;;  Converted to Clojure1.4 by Martin Trojer 2012.
 
 (ns bacwn.datalog.impl.magic
-  (:use bacwn.datalog.impl.util
-        bacwn.datalog.impl.literals
-        bacwn.datalog.impl.rules)
-  (:use [clojure.set :only (union intersection difference)]))
+  (:require [bacwn.datalog.impl.util :as util]
+            [bacwn.datalog.impl.literals :as literal]
+            [bacwn.datalog.impl.rules :as rule]
+            clojure.set))
 
 ;; =============================
 ;; Adornment
@@ -26,34 +26,34 @@
 (defn adorn-query
   "Adorn a query"
   [q]
-  (adorned-literal q (get-self-bound-cs q)))
+  (literal/adorned-literal q (literal/get-self-bound-cs q)))
 
 (defn adorn-rules-set
   "Adorns the given rules-set for the given query.  (rs) is a
    rules-set, (q) is an adorned query."
   [rs q]
-  (let [i-preds (all-predicates rs)
-        p-map (predicate-map rs)]
-    (loop [nrs empty-rules-set ; The rules set being built
-           needed #{(literal-predicate q)}]
+  (let [i-preds (rule/all-predicates rs)
+        p-map (rule/predicate-map rs)]
+    (loop [nrs rule/empty-rules-set ; The rules set being built
+           needed #{(literal/literal-predicate q)}]
       (if (empty? needed)
         nrs
         (let [pred (first needed)
               remaining (disj needed pred)
-              base-pred (get-base-predicate pred)
-              bindings (get-adorned-bindings pred)
+              base-pred (literal/get-base-predicate pred)
+              bindings (literal/get-adorned-bindings pred)
               new-rules (p-map base-pred)
-              new-adorned-rules (map (partial compute-sip bindings i-preds)
+              new-adorned-rules (map (partial rule/compute-sip bindings i-preds)
                                      new-rules)
               new-nrs (reduce conj nrs new-adorned-rules)
-              current-preds (all-predicates new-nrs)
+              current-preds (rule/all-predicates new-nrs)
               not-needed? (fn [pred]
                             (or (current-preds pred)
-                                (-> pred get-base-predicate i-preds not)))
+                                (-> pred literal/get-base-predicate i-preds not)))
               add-pred (fn [np pred]
                          (if (not-needed? pred) np (conj np pred)))
               add-preds (fn [np rule]
-                          (reduce add-pred np (map literal-predicate (:body rule))))
+                          (reduce add-pred np (map literal/literal-predicate (:body rule))))
               new-needed (reduce add-preds remaining new-adorned-rules)]
           (recur new-nrs new-needed))))))
 
@@ -65,25 +65,25 @@
   "Given a magic form of a query, give back the literal form of its seed
    relation"
   [q]
-  (let [pred (-> q literal-predicate get-base-predicate)
-        bnds (-> q literal-predicate get-adorned-bindings)]
+  (let [pred (-> q literal/literal-predicate literal/get-base-predicate)
+        bnds (-> q literal/literal-predicate literal/get-adorned-bindings)]
     (with-meta (assoc q :predicate [pred :magic-seed bnds]) {})))
 
 (defn seed-rule
   "Given an adorned query, give back its seed rule"
   [q]
-  (let [mq (build-seed-bindings (magic-literal q))
+  (let [mq (literal/build-seed-bindings (literal/magic-literal q))
         sr (seed-relation mq)]
-    (build-rule mq [sr])))
+    (rule/build-rule mq [sr])))
 
 (defn build-partial-tuple
   "Given a query and a set of bindings, build a partial tuple needed
    to extract the relation from the database."
   [q bindings]
   (into {} (remove nil? (map (fn [[k v :as pair]]
-                               (if (is-var? v)
+                               (if (util/is-var? v)
                                  nil
-                                 (if (is-query-var? v)
+                                 (if (util/is-query-var? v)
                                    [k (bindings v)]
                                    pair)))
                              (:term-bindings q)))))
@@ -101,25 +101,26 @@
    (i-preds) are the predicates of the intension database.  These
    default to the predicates within the rules-set."
   ([rs]
-     (magic-transform rs (all-predicates rs)))
+     (magic-transform rs (rule/all-predicates rs)))
   ([rs i-preds]
      (let [not-duplicate? (fn [l mh bd]
                             (or (not (empty? bd))
-                                (not (= (magic-literal l)
+                                (not (= (literal/magic-literal l)
                                         mh))))
            xr (fn [rs rule]
                 (let [head (:head rule)
                       body (:body rule)
-                      mh (magic-literal head)
-                      answer-rule (build-rule head
-                                              (concat [mh] body))
+                      mh (literal/magic-literal head)
+                      answer-rule (rule/build-rule head
+                                                   (concat [mh] body))
                       step (fn [[rs bd] l]
-                             (if (and (i-preds (literal-predicate l))
+                             (if (and (i-preds (literal/literal-predicate l))
                                       (not-duplicate? l mh bd))
-                               (let [nr (build-rule (magic-literal l)
-                                                    (concat [mh] bd))]
+                               (let [nr (rule/build-rule (literal/magic-literal l)
+                                                         (concat [mh] bd))]
                                  [(conj rs nr) (conj bd l)])
                                [rs (conj bd l)]))
                       [nrs _] (reduce step [rs []] body)]
                   (conj nrs answer-rule)))]
-       (reduce xr empty-rules-set rs))))
+       (reduce xr rule/empty-rules-set rs))))
+
